4855fd98af3a97e9f86b7cdcb853e1794b95b00e
smartdev0218 <antonio.krijan0218@gmail.com>
2022-07-05 20:29:59 +0200

    fixed contract

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -104,15 +104,7 @@ contract NftMarketplace is ReentrancyGuard {
             revert NotApprovedForMarketplace();
         }
         s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
-        emit ItemListed(msg.sender, nftAddress, tokenId, price);
-    }
-
-    /*
-     * @notice Method for cancelling listing
-     * @param nftAddress Address of NFT contract
-     * @param tokenId Token ID of NFT
-     */
-   
+        emit I
 
     /*
      * @notice Method for buying listing
